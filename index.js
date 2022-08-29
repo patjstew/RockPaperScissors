3546a73fc4c72bc3dfd68adc58ad40001285a737
patjstew <stewpat4@me.com>
2022-08-29 19:10:09 -0400

    create functions for game play-age

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 function getComputerChoice() {
     let a = "rock";
     let b = "paper";
@@ -13,3 +15,31 @@ function getComputerChoice() {
 }
 
 getComputerChoice()
+
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "rock") {
+        return "It's a tie!"
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You lose, paper beats rock."
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win, rock beats scissors."
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win! Paper beats rock."
+    } else if (playerSelection === "paper" && computerSelection === "paper") {
+        return "It's a tie!"
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You lose. Scissors beat paper."
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You lose! Rock beats scissors."
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win. Scissors beats paper."
+    } else if (playerSelection === "scissors" && computerSelection === "scissors") {
+        return "It's a tie!"
+    }
+    console.log(playRound(playerSelection, computerSelection));
+} 
+const playerSelection = prompt("'Rock, Paper, Scissors?'").toLowerCase();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
